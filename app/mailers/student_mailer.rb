03418ff0ad8c98dbeb5bad406a205122b331e0e9
Meredith Salguero <salguero.meredith@gmail.com>
2015-12-21 14:41:23 -0500

    testing production mailer

--- app/mailers/student_mailer.rb
+++ app/mailers/student_mailer.rb
@@ -7,11 +7,5 @@ class StudentMailer < ApplicationMailer
 		@password = password
 		@url = 'https://www.google.com'
 		mail(to: @user.email, subject: 'Welcome to ' + @cohort.cohort_name)
-		# mail = SendGrid::Mail.new do |m|
-		# 	m.to = "#{@user.email}"
-		# 	m.from = "testingattendence@gmail.com"
-		# 	m.subject = "Welcome to #{@cohort.cohort_name}"
-		# 	m.text = "I heard you like pineapple"
-		# end
 	end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -85,8 +85,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'www.gmail.com', # might be gmail.com
+    :domain => 'https://floating-mesa-1461.herokuapp.com', # might be gmail.com
     :enable_starttls_auto => true
   }
-  # client = SendGrid::Client.new(api_user: ENV['SENDGRID_USERNAME'], api_key: ENV['SENDGRID_PASSWORD'])
 end
