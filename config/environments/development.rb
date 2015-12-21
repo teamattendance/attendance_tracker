a918d60fdb573d62934ca85f4092048390763cb1
Meredith Salguero <salguero.meredith@gmail.com>
2015-12-21 11:58:34 -0500

    changed authentication plain to string

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,11 +57,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => 'smtp.sendgrid.net',
-    :port => '587',
+    :port => 587,
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
+    :domain => 'floating-mesa-1461.herokuapp.com', # might be gmail.com
     :enable_starttls_auto => true
   }
 end
