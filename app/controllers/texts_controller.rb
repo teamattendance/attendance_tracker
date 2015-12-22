29762985a9cecba518c9497e4f66762972f2a86b
Meredith Salguero <salguero.meredith@gmail.com>
2015-12-21 20:00:24 -0500

    users have been found

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -16,15 +16,14 @@ class TextsController <ApplicationController
 
     # SMSLogger.log_text_message from_number, message_body
     @user = User.find_by({phone: from_number})
-    puts @user.first_name
-    # message_array = message_body.split(" ")
-    # message_array.each do |mess|
-    # 	if mess.start_with?("sick")
-    # 		@user.date_records.where(day: Time.now.to_s.split(" ")[0].to_date).attendence = "excused"
-    # 	elsif mess.start_with?("late")
-    # 		@user.date_records.where(day: Time.now.to_s.split(" ")[0].to_date).attendence = "late"
-    # 	end
-    # end
+    message_array = message_body.split(" ")
+    message_array.each do |mess|
+    	if mess.start_with?("sick")
+    		@user.date_records.where(day: Time.now.to_s.split(" ")[0].to_date).attendence = "excused"
+    	elsif mess.start_with?("late")
+    		@user.date_records.where(day: Time.now.to_s.split(" ")[0].to_date).attendence = "late"
+    	end
+    end
 
     redirect_to "/"
 	end
