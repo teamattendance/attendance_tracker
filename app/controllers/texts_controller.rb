4ada6a6c88d86318fcb3e1c9d911dde86044a5cf
Meredith Salguero <salguero.meredith@gmail.com>
2015-12-21 19:12:45 -0500

    reseeded

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -16,7 +16,7 @@ class TextsController <ApplicationController
 
     # SMSLogger.log_text_message from_number, message_body
     @user = User.find_by(phone: from_number)
-    puts @user.first_name
+    puts @user.first.first_name
     # message_array = message_body.split(" ")
     # message_array.each do |mess|
     # 	if mess.start_with?("sick")
